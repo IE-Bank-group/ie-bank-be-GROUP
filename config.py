34bb756e38567e5a180b14d3a58c197323f006e1
juandifers <133695239+juandifers@users.noreply.github.com>
2024-12-08 20:19:12 +0100

    Final commit

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+import urllib.parse
+from azure.identity import DefaultAzureCredential
 
 class Config(object): 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'dev-secret-key')
 
 class LocalConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
@@ -13,19 +16,37 @@ class GithubCIConfig(Config):
     DEBUG = True
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )
     DEBUG = True
 
+    def __init__(self):
+        credential = DefaultAzureCredential()
+        dbuser = urllib.parse.quote(os.getenv('DBUSER', 'default_user'))
+        dbpass = credential.get_token(
+            'https://ossrdbms-aad.database.windows.net').token
+        dbhost = os.getenv('DBHOST', 'localhost')
+        dbname = os.getenv('DBNAME', 'development_db')
+        self.SQLALCHEMY_DATABASE_URI = f'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'
+
 class UATConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )
     DEBUG = True
+
+    def __init__(self):
+        credential = DefaultAzureCredential()
+        dbuser = urllib.parse.quote(os.getenv('DBUSER', 'default_user'))
+        dbpass = credential.get_token(
+            'https://ossrdbms-aad.database.windows.net').token
+        dbhost = os.getenv('DBHOST', 'localhost')
+        dbname = os.getenv('DBNAME', 'uat_db')
+        self.SQLALCHEMY_DATABASE_URI = f'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'
+        
+class ProductionConfig(Config):
+    DEBUG = False
+
+    def __init__(self):
+        credential = DefaultAzureCredential()
+        dbuser = urllib.parse.quote(os.getenv('DBUSER', 'default_user'))
+        dbpass = credential.get_token(
+            'https://ossrdbms-aad.database.windows.net').token
+        dbhost = os.getenv('DBHOST', 'localhost')
+        dbname = os.getenv('DBNAME', 'production_db')
+        self.SQLALCHEMY_DATABASE_URI = f'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'
--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -9,8 +9,11 @@ import os
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 load_dotenv()  
 # Configure secrets
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback-secret-key')
@@ -23,7 +26,7 @@ bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 
-if os.getenv('ENV') == 'production':
+'''if os.getenv('ENV') == 'production':
     CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:8080"}})
 else:
     CORS(app, supports_credentials=True, resources={
@@ -33,7 +36,9 @@ else:
             "expose_headers": ["Access-Control-Allow-Origin"],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
         }
-    })
+    })'''
+    
+   
 
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'local':
@@ -67,3 +72,40 @@ from iebank_api import routes
 # Debugging URL Map
 print(app.url_map)
 print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
+def create_admin_user():
+    with app.app_context():
+        # Define the admin user details
+        username = 'adminuser'
+        password_hash = 'adminpassword'
+        email = 'adminuser@example.com'
+        date_of_birth = '2004-02-28'
+        admin = True
+
+        # Hash the password
+        hashed_password = generate_password_hash(password_hash, method='pbkdf2:sha256')
+
+        # Convert date_of_birth to a datetime object
+        date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')
+
+        # Create the new admin user
+        new_user = User(
+            username=username,
+            password_hash=hashed_password,
+            email=email,
+            date_of_birth=date_of_birth,
+            admin=admin
+        )
+
+        # Add the new user to the database
+        # if the new user does not already exist
+        if User.query.filter_by(username=username).first():
+            print(f"Admin user '{username}' already exists.")
+            return
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        print(f"Admin user '{username}' created successfully.")
+
+create_admin_user()
