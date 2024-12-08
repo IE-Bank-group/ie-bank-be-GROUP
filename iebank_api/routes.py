e270bb477c021e2f4a1df32b19a0c7dff25c367f
juandifers <133695239+juandifers@users.noreply.github.com>
2024-12-08 16:21:03 +0100

    h

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -38,7 +38,7 @@ def skull():
 
 
 @app.route('/register', methods=['POST'])
-#@cross_origin(supports_credentials=True, origins=["http://localhost:8080"])
+@cross_origin(supports_credentials=True, origins=["http://localhost:8080"])
 def register():
     """Register a new user"""
     data = request.get_json()
